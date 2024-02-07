fdcedb6385431889b9e4aa42f1d0d4f393f133c8
Abhilash Srivastava <srivastava.abhilash26@gmail.com>
2024-02-07 22:08:10 +0530

    fix: instead of component rerender, only return updated value

--- internal/handler/counterHandler.go
+++ internal/handler/counterHandler.go
@@ -1,29 +1,23 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/abhilash26/gohyper/internal/model"
 	"github.com/abhilash26/gohyper/internal/storage"
-	"github.com/abhilash26/gohyper/internal/view"
 )
 
 func CounterAdd(w http.ResponseWriter, r *http.Request) {
-  counter, _ := model.GetCounter(storage.DB)
-  counter++;
+	counter, _ := model.GetCounter(storage.DB)
+	counter++
 	model.UpdateCounterValue(storage.DB, counter)
-	templates := []string{
-		"component/counter",
-	}
-	view.RenderTemplate(w, templates, "counter", counter)
+	fmt.Fprintf(w, "%d", counter)
 }
 
 func CounterSubtract(w http.ResponseWriter, r *http.Request) {
-  counter, _ := model.GetCounter(storage.DB)
-  counter--;
+	counter, _ := model.GetCounter(storage.DB)
+	counter--
 	model.UpdateCounterValue(storage.DB, counter)
-	templates := []string{
-		"component/counter",
-	}
-	view.RenderTemplate(w, templates, "counter", counter)
+	fmt.Fprintf(w, "%d", counter)
 }
